9538e778a3bdde9a488905da60066692f63ff43f
LaBibliaTeca <gustavo.uliarte@hotmail.com>
2025-08-08 15:21:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,18 @@ def transliterate_modern_greek_v20(text_koine):
 
 # Interfaz Streamlit
 st.title("Transliterador de Griego Koiné a Pronunciación Moderna")
-st.markdown("**Por Gustavo Uliarte **")
+st.markdown(
+    """
+    <p style="font-size:16px;">
+        <strong>Por Gustavo Fernández</strong>
+        <a href="https://www.facebook.com/Asistente3Juan15/" target="_blank" style="text-decoration:none;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" 
+                 alt="Facebook" width="20" style="vertical-align:middle; margin-left:10px;">
+        </a>
+    </p>
+    """,
+    unsafe_allow_html=True
+)
 input_text = st.text_area("Pega el texto griego aquí:", height=200)
 
 if st.button("Transliterar"):
